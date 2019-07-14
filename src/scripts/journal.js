a311c85d1854699e7ce9a77e1c3697f11c929675
Curtis Crutchfield <crutchc@yahoo.com>
2019-07-14 13:55:00 -0500

    added 4 journal entries in seprate objects used .push to add them to an array that will hold all entries

---
+++ src/scripts/journal.js
@@ -0,0 +1,36 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    JournalDate: "07/12/2019",
+    ConceptsCovered: "JavaScript Objects",
+    JournalEntry: 'Today we went over Book 2 Chapter 2 "JavaScript Objects". We practiced objects doing the lighting exercise in the chapter. I understand the difference between objects{} and arrays[]. Also learned about Dot and Bracket notation',
+    Mood: "Meh"
+}
+const journalEntryTwo = {
+    JournalDate: "07/12/2019",
+    ConceptsCovered: "Querying the Dom",
+    JournalEntry: 'Went over the queryselector and how it can be used to change items in the html without writing additional html code. Set up a class in CSS and you are able to manipulate the HTML with Javascript',
+    Mood: "Sappy"
+}
+const journalEntryThree = {
+    JournalDate: "07/11/2019",
+    ConceptsCovered: "Daily Journal",
+    JournalEntry: 'Built and styled the 1st part of the daily journal',
+    Mood: "Glorious"
+}
+const journalEntryFour = {
+    JournalDate: "07/11/2019",
+    ConceptsCovered: "Flexbox",
+    JournalEntry: 'Went over the Flexbox, and how it can be used to style pages, was able to complete the coffehouse practice!',
+    Mood: "Glorious"
+}
+
+const journalEntries = []
+journalEntries.push(journalEntry)
+journalEntries.push(journalEntryTwo)
+journalEntries.push(journalEntryThree)
+journalEntries.push(journalEntryFour)
+
+console.log(journalEntries)
