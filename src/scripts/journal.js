d384ab90bb695a86673826b6b7082012a4335f09
Curtis Crutchfield <crutchc@yahoo.com>
2019-07-21 23:13:01 -0500

    Added javascript functions to insert html into the dom, and added a card flexbox styling to the info

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,7 +2,7 @@
     Define the keys and value for a JavaScript object that
     represents a journal entry about what you learned today
 */
-const journalEntry = [
+const journalEntries = [
     {
         JournalDate: "07/12/2019",
         ConceptsCovered: "JavaScript Objects",
@@ -26,9 +26,49 @@ const journalEntry = [
         ConceptsCovered: "Flexbox",
         JournalEntry: 'Went over the Flexbox, and how it can be used to style pages, was able to complete the coffehouse practice!',
         Mood: "Glorious"
+    },
+    {
+        JournalDate: "07/19/2019",
+        ConceptsCovered: "API's",
+        JournalEntry: "Learned about API's(application program interface). Really cool but a little confusing.",
+        Mood: "Meh"
+    },
+    {
+        JournalDate: "07/21/2019",
+        ConceptsCovered: "Daily Journal Part 3",
+        JournalEntry: 'Completed the 3rd step of the daily journal fairly easy, wrote a function that inserts my html info into the DOM!',
+        Mood: "Glorious"
     }
-]
-const journalEntries = []
-journalEntries.push(journalEntry)
 
-console.log(journalEntries)
+]
+const journalEntryArr = []
+journalEntryArr.push(journalEntries)
+
+console.log(journalEntryArr)
+
+/*
+    Purpose: To render all journal entries to the DOM
+
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (JournalDate, ConceptsCovered, JournalEntry, Mood) => {
+    return `
+        <div class="journal-card">
+        <h2>${JournalDate}</h2>
+        <h3>${ConceptsCovered}</h3>
+        <aside>${JournalEntry}</aside>
+        <h4>Mood: ${Mood}</h4>
+        </div>
+    `
+}
+
+// Invoke the render function
+renderJournalEntries(journalEntries)
+
+// insert into the html
+const journalDom = document.querySelector('.entryLog')
+
+journalEntries.forEach(journalEntries => {
+    const insertHtml = renderJournalEntries(journalEntries.JournalDate, journalEntries.ConceptsCovered, journalEntries.JournalEntry, journalEntries.Mood)
+    journalDom.innerHTML += insertHtml
+})
